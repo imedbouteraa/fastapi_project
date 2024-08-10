1d8700a660d5608909dc60994316506a4a9cfdc7
imed bouteraa <imadboutera@gmail.com>
2024-08-10 12:03:40 +0100

    second commit

--- static/js/idcard.js
+++ static/js/idcard.js
@@ -12,10 +12,23 @@ document.getElementById('ocr-form').onsubmit = async function(e) {
         });
 
         const result = await response.json();
-        document.getElementById('result').innerHTML = JSON.stringify(result, null, 2);
+        document.getElementById('result').innerHTML = formatResultAsTable(result);
     } catch (error) {
         console.error("There was an error processing the image!", error);
     } finally {
         document.getElementById('loading').style.display = 'none';
     }
 };
+
+function formatResultAsTable(data) {
+    let table = '<table class="result-table">';
+    table += '<tr><th>Field</th><th>Value</th></tr>';
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            table += `<tr><td>${key.replace('_', ' ').toUpperCase()}</td><td>${data[key]}</td></tr>`;
+        }
+    }
+    table += '</table>';
+    return table;
+}
+
